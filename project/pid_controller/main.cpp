8e30a8c39aab4d38e46f2803a2d261acb1944211
Sune Kaae <sunekaae@gmail.com>
2026-01-05 10:09:02 +0100

    simpler init

--- project/pid_controller/main.cpp
+++ project/pid_controller/main.cpp
@@ -226,9 +226,9 @@ int main ()
   **/
 
   PID pid_steer = PID();
-  pid_steer.Init(0.12, 0.02, 2.0, 1.0, -1.0);
+  pid_steer.Init(0.12, 0.0, 0.0, 1.0, -1.0);
   PID pid_throttle = PID();
-  pid_throttle.Init(0.4, 0.15, 0.0, 1.0, -1.0);
+  pid_throttle.Init(0.4, 0.0, 0.0, 1.0, -1.0);
 
   h.onMessage([&pid_steer, &pid_throttle, &new_delta_time, &timer, &prev_timer, &i, &prev_timer](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode)
   {
